254fab701e50148e6aa41ab8ce1184a6489bca5a
Geoov <george.geov7@gmail.com>
2022-04-16 19:29:42 -0700

    finish company dashboard

--- src/app/static/dashboard/components/company-details/company-details.component.ts
+++ src/app/static/dashboard/components/company-details/company-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CompanyDetails } from 'src/app/core/models/company-details';
+import { AuthService } from 'src/app/core/services/authentication/auth.service';
+import { CompanyService } from 'src/app/core/services/company/company.service';
 
 @Component({
     selector: 'app-company-details',
@@ -7,9 +12,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CompanyDetailsComponent implements OnInit {
     @Input() idCompany: number = -1;
-    constructor() { }
+    company: CompanyDetails = {};
+    companyDetailsForm = new FormGroup({
+        company_name: new FormControl(),
+        tehnologii: new FormControl(),
+        an_start: new FormControl(),
+        nr_angajati: new FormControl(),
+        descriere: new FormControl()
+     });
+    constructor(
+        private route: Router,
+        private companyService: CompanyService,
+        private authService: AuthService
+    ) { }
 
     ngOnInit(): void {
+        console.log(this.idCompany);
+
+        if (this.idCompany !== -1) {
+            this.companyService.getCompanyById(this.idCompany).subscribe((company: CompanyDetails) => {
+                this.company = company;;
+                console.log(this.company);
+            })
+        }
+    }
+
+    onSubmit(): void {
+
     }
 
 }
